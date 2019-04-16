5c60399e92d60b2e164cfd9e93cb9bf6edd63998
Rider Jensen <riderjensen@gmail.com>
2019-04-16 09:44:56 -0600

    add get one route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import UsableHome from './components/UsableHome'
 import UsableRelated from './components/UsableRelated'
+import UsableOne from './components/UsableOne'
 
 
 export const routes = [
     { path: '', component: UsableHome },
     { path: '/related', component: UsableRelated },
+    { path: '/:id', component: UsableOne },
 ];
